5a7bd159df140952e0c3d59f950829202d986852
Necromancy501 <94140246+Necromancy501@users.noreply.github.com.>
2024-12-15 19:29:46 +0800

    Fix feedback behaviour

--- lib/ruleset.rb
+++ lib/ruleset.rb
@@ -1,5 +1,4 @@
-#GUESS CHECK LOGIC NEEDED
-#ITERATOR BEHAVIOR CHANGE
+
 
 class Ruleset
 
@@ -33,13 +32,16 @@ class Ruleset
     flag_counter = @@z
     exact_matches = []
     secret_matches = []
-    choice_copy = @@choice_arr
+    win_counter = 0
+    quit_counter = 1
+    choice_copy = b = @@choice_arr.map(&:clone)
 
     while @@z < Board.status[:pegs_placed]
         if $guess_array[@@i] == @@choice_arr[@@i]
           Flag.array[flag_counter][:color] = Gosu::Color.argb(255, 67, 255, 231)
           flag_counter +=1
-          choice_copy -= [$guess_array[@@i]]
+          win_counter += 1
+          choice_copy[@@i] = 0
           exact_matches.push(@@i)
         end
         @@i+=1
@@ -47,14 +49,16 @@ class Ruleset
     end
 
     while @@x < Board.status[:pegs_placed]
-      if flag_counter ==  @@z
+      if win_counter ==  4 && quit_counter == 1
         Ruleset.win
+        quit_counter = 0
       end
       unless exact_matches.include?(@@y) || secret_matches.include?(@@y)
         if choice_copy.include?($guess_array[@@y])
           Flag.array[flag_counter][:color] = Gosu::Color.argb(255, 255, 221, 165)
           flag_counter += 1
-          choice_copy -= [$guess_array[@@y]]
+          first_ocurrence = choice_copy.find_index($guess_array[@@y])
+          choice_copy[first_ocurrence] = 0
           secret_matches.push(@@y)
         end
       end
